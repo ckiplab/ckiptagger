e89a1460116dd397ae9cf550936a9b0ebe73b18e
circlelychen <truecirclely@gmail.com>
2019-12-25 15:00:57 +0800

    tensorflow is installed_required

--- setup.py
+++ setup.py
@@ -2,26 +2,28 @@ import setuptools
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
-    
+
 setuptools.setup(
-    name = "ckiptagger",
-    version = "0.1.0",
-    author = "Peng-Hsuan Li",
-    author_email = "jacobvsdanniel@gmail.com",
-    description = "Neural implementation of CKIP WS, POS, NER tools",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/ckiplab/ckiptagger",
-    python_requires = ">=3.6",
-    packages = ["ckiptagger"],
-    package_dir = {"ckiptagger": "src"},
-    extras_require = {
-        "tf": ["tensorflow>=1.13.1,<2"],
+    name="ckiptagger",
+    version="0.1.0",
+    author="Peng-Hsuan Li",
+    author_email="jacobvsdanniel@gmail.com",
+    description="Neural implementation of CKIP WS, POS, NER tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ckiplab/ckiptagger",
+    python_requires=">=3.6",
+    packages=["ckiptagger"],
+    package_dir={"ckiptagger": "src"},
+    install_requires=[
+        "tf": ["tensorflow>=1.13.1,<2"]
+    ],
+    extras_require={
         "tfgpu": ["tensorflow-gpu>=1.13.1,<2"],
         "gdown": ["gdown"],
     },
-    license = "GPLv3",
-    classifiers = [
+    license="GPLv3",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
